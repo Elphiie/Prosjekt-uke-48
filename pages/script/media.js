757bdffe856b62ebe4843f0ddc94ec373def94ae
Elphiie <alven-erlend@Hotmail.com>
2021-11-30 22:51:21 +0100

    Did some scripting. Send help

--- pages/script/media.js
+++ pages/script/media.js
@@ -1,22 +1,47 @@
-var myAudio = document.getElementById('audio');
+var ulBtn = document.getElementById('ulBtn')
+var loadup = document.getElementById('form')
+ulBtn.addEventListener('click', () =>{
 
-function playAudio() {
-    myAudio.play();
+    if (loadup.style.display === 'none', ulBtn.style.display === 'flex'){
+        loadup.style.display = 'block', ulBtn.style.display = 'none';
+    } else {
+        loadup.style.display = 'none', ulBtn.style.display = 'flex';
+    }
+})
+
+    
+
+const getValuesFromInputs = () =>{
+    const profileName = document.querySelector('input.profile-name').value;
+    const profileImg = document.querySelector('input.profile-img').files[0];
+    const profileAud = document.querySelector('input.profile-aud').files[0];
+ 
+    document.querySelector('form').style.display = 'none';
+ 
+    return [profileName, profileImg, profileAud];
+ 
+ }
+
+ const convertInputValues = () => {
+    const [profileName, profileImg, profileAud] = getValuesFromInputs();
+  
+    const profileImgURL = URL.createObjectURL(profileImg);
+    const profileAudURL = URL.createObjectURL(profileAud);
+  
+    return[profileAudURL, profileImgURL, profileName ]
+  }
+
+  const addInputToProfile = () => {
+
+    const [profileAudURL, profileImgURL, profileName ]  = convertInputValues();
+
+    document.querySelector('.audio h2').innerHTML = `${profileName}`;
+    document.querySelector('.audio img').setAttribute('src', profileImgURL);
+    document.querySelector('.aud').setAttribute('src', profileAudURL);
+
 }
 
-function pauseAudio() {
-    myAudio.pause();
-}
-
-function dlAudio(filename, text){
-    var element = document.createElement('a');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-    element.setAttribute('dlAudio', "../audio/.mp3");
-
-        element.style.display = 'none';
-        document.body.appendChild(element);
-
-        element.click();
-
-        document.body.removeChild(element);
-}
+document.querySelector('.btn').addEventListener('click', (e) => {
+    e.preventDefault();
+    addInputToProfile();
+  });
