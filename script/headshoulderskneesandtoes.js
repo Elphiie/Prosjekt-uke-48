98b1729ba66a72b6ed5ad13a27561bda3c9f7f38
Elphiie <alven-erlend@Hotmail.com>
2021-11-30 12:46:35 +0100

    script

---
+++ script/headshoulderskneesandtoes.js
@@ -0,0 +1,9 @@
+
+var header_content = '<!-- Code for header---> ;
+var footer_content = '<!-- Code for footer---> ;
+
+var header_element = document.getElementById('header');
+var footer_element = document.getElementById('footer');
+
+header_element.innerHTML = header_content;
+footer_element.innerHTML = footer_content;
