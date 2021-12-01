3bc4af276b3784242ed9470f996433c866d7d495
Elphiie <alven-erlend@Hotmail.com>
2021-12-01 01:42:01 +0100

    Forsøker å legge til en next knapp til opplastingsfeltene

--- pages/script/media.js
+++ pages/script/media.js
@@ -1,7 +1,6 @@
 var ulBtn = document.getElementById('ulBtn')
 var loadup = document.getElementById('form')
 ulBtn.addEventListener('click', () =>{
-
     if (loadup.style.display === 'none', ulBtn.style.display === 'flex'){
         loadup.style.display = 'block', ulBtn.style.display = 'none';
     } else {
@@ -9,12 +8,17 @@ ulBtn.addEventListener('click', () =>{
     }
 })
 
-    
+
+var sampleName = document.getElementById('field1').id=1
+var ulImg = document.getElementById('field2').id=2
+var ulAudio = document.getElementById('field3').id=3
+var nxtBtn = document.getElementById('nextBtn')
+
 
 const getValuesFromInputs = () =>{
-    const profileName = document.querySelector('input.profile-name').value;
-    const profileImg = document.querySelector('input.profile-img').files[0];
-    const profileAud = document.querySelector('input.profile-aud').files[0];
+    const profileName = document.querySelector('input.sample-name').value;
+    const profileImg = document.querySelector('input.cover-img').files[0];
+    const profileAud = document.querySelector('input.sample-aud').files[0];
  
     document.querySelector('form').style.display = 'none';
  
@@ -42,6 +46,11 @@ const getValuesFromInputs = () =>{
 }
 
 document.querySelector('.btn').addEventListener('click', (e) => {
+    if (ulBtn.style.display === 'none'){
+        ulBtn.style.display = 'flex';
+    } else {
+        ulBtn.style.display = 'none';
+    }
     e.preventDefault();
     addInputToProfile();
   });
