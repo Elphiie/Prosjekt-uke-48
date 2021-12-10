05ee20016e99ce41506acfc7f731f58821aff7db
Elphiie <alven-erlend@Hotmail.com>
2021-12-10 18:47:26 +0100

    Merged from test

--- pages/script/test.js
+++
@@ -1,7 +0,0 @@
-function handleFiles(event) {
-    var files = event.target.files;
-    $("#src").attr("src", URL.createObjectURL(files[0]));
-    document.getElementById("audio").load();
-}
-
-document.getElementById("upload").addEventListener("change", handleFiles, false);
